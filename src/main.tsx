18339a76d64b496a743b545d509ccd09177df449
reginaldo miranda <reginaldobrain@gmail.coma>
2025-04-05 18:39:16 -0300

    inseriu fontes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { GlobalStyle } from './styles/globalStyle.ts'
 
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <GlobalStyle />
     <App />
   </StrictMode>,
 )
---
+++ src/styles/globalStyle.ts
@@ -0,0 +1,17 @@
+
+
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+   body, html {
+     margin : 0;
+     padding : 0;
+     font-family:  "Inter", sans-serif;
+
+   }
+  h1,h2, p, ul,li {
+    margin: 0;
+    padding: 0;
+    }
+
+`
