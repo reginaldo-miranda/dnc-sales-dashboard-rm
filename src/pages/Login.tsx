877a6fab9778631db9ed3cc33295cc66676cbdb1
reginaldo miranda <reginaldobrain@gmail.coma>
2025-04-12 18:03:59 -0300

    logim

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,25 +1,82 @@
-import styled from "styled-components";
+/*import styled from 'styled-components'
+import { Box, Container, Grid } from '@mui/material'
+
+//import { display } from 'node_modules/@mui/system/esm/Box/Box'
 
 const LoginArea = styled.div`
   background: #666;
 `
 const LoginImage = styled.div`
- background: url(login-image.svg);
- background-size: cover;
- height: 100vh;
- width: 50vw;
+  background: url(login-image.svg);
+  background-size: cover;
+  height: 100vh;
+  width: 50vw;
 `
 
 function Login() {
- 
+  return (
+    <>
+      <Box>
+        <Grid container>
+          <Grid
+            item
+            xs={12}
+            sm={6}
+            sx={{ alignItems: 'center', display: 'flex', height: '100vh' }}
+          >
+            <Container maxWidth="sm">
+              <LoginArea>Login</LoginArea>
+            </Container>
+          </Grid>
+        </Grid>
+        <Grid item sm={6} sx={{ display: { xs: 'none', sm: 'block' } }}>
+          <LoginImage />
+        </Grid>
+      </Box>
+    </>
+  )
+}
 
-    return (
-     <>
-      <LoginArea>Login</LoginArea>
-      <LoginImage/>
-     
-     </>
-    )
-  }
+export default Login
 
-  export default Login;
+*/
+//---------------------------------------
+import styled from 'styled-components'
+import { Box, Container, Grid } from '@mui/material'
+
+const LoginArea = styled.div`
+  background: #666;
+`
+
+const LoginImage = styled.div`
+  background: url(login-image.svg);
+  background-size: cover;
+  height: 100vh;
+  width: 50vw;
+`
+
+function Login() {
+  return (
+    <>
+      <Box>
+        <Grid container>
+          <Grid
+            item
+            xs={12}
+            sm={6}
+            sx={{ alignItems: 'center', display: 'flex', height: '100vh' }}
+          >
+            <Container maxWidth="sm">
+              <LoginArea>Login</LoginArea>
+            </Container>
+          </Grid>
+          <Grid item sm={6} sx={{ display: { xs: 'none ', sm: 'block' } }}>
+            <LoginImage />
+          </Grid>
+        </Grid>
+      </Box>
+    </>
+  )
+}
+
+export default Login
