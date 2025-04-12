f084b161cf1422e6a5939fd17cae580e11d5fef9
reginaldo miranda <reginaldobrain@gmail.coma>
2025-04-12 09:23:41 -0300

    acertar main

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
-/*import { StrictMode } from 'react'
+//364
+
+import { StrictMode } from 'react'
 import { ThemeProvider } from 'styled-components'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 
 import App from './App.tsx'
 import { darkTheme, GlobalStyle, lightTheme } from './styles/'
 
 //import { darkTheme, GlobalStyle, lightTheme } from './styles/';
 
-createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider theme={lightTheme}>
       <GlobalStyle />
@@ -15,7 +17,8 @@ createRoot(document.getElementById('root')!).render(
     </ThemeProvider>
   </StrictMode>
 )
-*/
+
+/*
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
@@ -36,3 +39,4 @@ if (rootElement) {
     </StrictMode>
   )
 }
+*/
