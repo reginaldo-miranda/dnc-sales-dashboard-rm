8c6a08b053def49f385b0cc7e65d2bd92b49a99e
reginaldo miranda <reginaldobrain@gmail.coma>
2025-04-12 21:27:52 -0300

    acertar paginas

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,48 +1,8 @@
-/*import styled from 'styled-components'
-import { Box, Container, Grid } from '@mui/material'
-
 //import { display } from 'node_modules/@mui/system/esm/Box/Box'
 
-const LoginArea = styled.div`
-  background: #666;
-`
-const LoginImage = styled.div`
-  background: url(login-image.svg);
-  background-size: cover;
-  height: 100vh;
-  width: 50vw;
-`
-
-function Login() {
-  return (
-    <>
-      <Box>
-        <Grid container>
-          <Grid
-            item
-            xs={12}
-            sm={6}
-            sx={{ alignItems: 'center', display: 'flex', height: '100vh' }}
-          >
-            <Container maxWidth="sm">
-              <LoginArea>Login</LoginArea>
-            </Container>
-          </Grid>
-        </Grid>
-        <Grid item sm={6} sx={{ display: { xs: 'none', sm: 'block' } }}>
-          <LoginImage />
-        </Grid>
-      </Box>
-    </>
-  )
-}
-
-export default Login
-
-*/
-//---------------------------------------
 import styled from 'styled-components'
 import { Box, Container, Grid } from '@mui/material'
+//import Grid from '@mui/material/Grid'
 
 const LoginArea = styled.div`
   background: #666;
