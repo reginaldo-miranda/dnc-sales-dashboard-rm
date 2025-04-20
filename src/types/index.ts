d30fdbe5993f7535e5f90b9672efa205fdb8dcf8
reginaldo miranda <reginaldobrain@gmail.coma>
2025-04-20 18:58:19 -0300

    feat: chart-components

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,6 @@
 export * from './avatarsList'
 export * from './customTable'
+export * from './customChart'
 export * from './theme'
 export * from './typographies'
 export * from './formComponents'
