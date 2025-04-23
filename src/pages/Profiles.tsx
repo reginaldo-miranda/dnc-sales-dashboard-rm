6fd09f7044a2a9909960605c578c13b123d13bcd
Reginaldo <reginaldobrain@gmail.com>
2025-04-23 10:25:16 -0300

    feat: logout-flow

--- src/pages/Profiles.tsx
+++ src/pages/Profiles.tsx
@@ -1,7 +1,13 @@
 import { useContext } from "react";
-import { CardComponents, Header, StyledButton} from "@/components"
+import { CardComponents, Header, StyledH2, StyledButton} from "@/components"
 import { AppThemeContext } from "@/context/AppThemeContext";
 
+// MUI
+
+import { Container, Grid } from "@mui/material";
+
+// services
+import {logout} from "@/services"
 
 function Profiles() {
   
@@ -10,12 +16,35 @@ function Profiles() {
     return (
      <>
      <Header />
+     <Container className="mb-2" maxWidth="lg">
+          <Grid container spacing={4}>
+            <Grid item xs={12} sm={6} >
+              <CardComponents>
+                Seus dados
+              </CardComponents>
+            </Grid>
+
+
+            <Grid item xs={12} sm={6} >
+              <CardComponents>
+                <StyledH2  className="mb-1">Definiçoes de Conta</StyledH2>
+                  <StyledButton className="primary mb-1"  onClick={themeContext?.toggleTheme}  >
+                    Trocar para tema{' '}
+                      {themeContext?.appTheme === 'light' ? 'escuro' : 'claro'}       
+                  </StyledButton>
+                  <StyledButton className='alert' onClick={logout} >
+                      Logout
+                  </StyledButton>
+
+              </CardComponents>
+            </Grid>
+
+          </Grid>
+              {/* Add your profile cards here */}
+     </Container>
    <CardComponents>
 
-    <StyledButton className="primary"  onClick={themeContext?.toggleTheme}  >
-       Trocar para tema{' '}
-        {themeContext?.appTheme === 'light' ? 'escuro' : 'claro'}       
-    </StyledButton>
+   
     </CardComponents>
      </>
     )
--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -40,6 +40,7 @@ export const GlobalStyle = createGlobalStyle<{ theme: Theme }>`
 `
 */
 
+import { pxToRem } from '@/utils'
 import { createGlobalStyle } from 'styled-components'
 import { DefaultTheme } from 'styled-components'
 
@@ -58,4 +59,15 @@ export const GlobalStyle = createGlobalStyle<{ theme?: DefaultTheme }>`
     margin: 0;
     padding: 0;
   }
+  .mb-1{
+  
+    margin-bottom: ${pxToRem(16)};
+  }
+
+  .mb-2{
+  
+    margin-bottom: ${pxToRem(32)};
+  }
+
+
 `
